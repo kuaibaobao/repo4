b1e6b34e1b1bf253484c9e202d5436d25b18160e
江薇 <1358016444@qq.com>
2019-02-21 10:11:58 +0800

    提交工程

---
+++ maven_day01_service/src/main/java/com/itheima/service/ItemsService.java
@@ -0,0 +1,10 @@
+package com.itheima.service;
+
+import com.itheima.domain.Items;
+
+public interface ItemsService {
+
+    Items findById(Integer id);
+
+
+}
---
+++ maven_day01_web/src/test/java/com/itheima/test.java
@@ -0,0 +1,9 @@
+package com.itheima;
+
+import org.junit.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class test {
+
+}
